725bed1ed226b955d970f27aaf36b799c4940f05
XxEngAbdullahxX <119155861+XxEngAbdullahxX@users.noreply.github.com>
2024-07-28 17:55:56 +0300

    made patient card

---
+++ lib/cards/patient_card.dart
@@ -0,0 +1,69 @@
+import 'package:b_dental/globals.dart';
+import 'package:flutter/material.dart';
+
+class PatientCard extends StatelessWidget {
+  final int id;
+  final String name;
+  final String phone;
+  final int sex;
+
+  const PatientCard(this.id, this.name, this.phone, this.sex, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: globalColorDarker,
+      margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: Row(
+        children: <Widget>[
+          const SizedBox(width: 10),
+          Icon(
+            sex == 1? Icons.person : Icons.person_2,
+            color: sex == 1? Colors.blue : Colors.pink,
+            size: 26,
+          ),
+          const SizedBox(width: 7),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: globalColorLight,
+                ),
+              ),
+              Text(
+                phone,
+                style: const TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 10)
+            ],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.info_outline,
+                    color: globalColorLight,
+                    size: 26,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          )
+        ],
+      ),
+    );
+  }
+}
