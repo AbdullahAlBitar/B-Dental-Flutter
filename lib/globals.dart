112b526021d198822e1a858ebdd86bdecdd56a88
XxEngAbdullahxX <119155861+XxEngAbdullahxX@users.noreply.github.com>
2024-07-28 17:56:31 +0300

    globle colors added

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 
-Color globalColorDark = Colors.cyan.shade700;
-Color globalColorLight = Colors.cyan;
+Color globalColorDarker =const Color.fromARGB(255, 0, 69, 75);
+Color globalColorDark = Colors.cyanAccent.shade700;
+Color globalColorLight =Colors.cyanAccent;
+
+Color globalBG = const Color.fromARGB(255, 59, 59, 59);
+Color globalDarkBG = const Color.fromARGB(255, 32, 32, 32);
